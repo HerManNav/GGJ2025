0c832d1328b32a40357b7497e139e05aeeebe1aa
Sergio Basurco <sergio.basurco@gmail.com>
2025-01-25 00:22:56 +0100

    starsh

---
+++ Source/GGJ2025/BubbleGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "BubbleGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGJ2025_API ABubbleGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
